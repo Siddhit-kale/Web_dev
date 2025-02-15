a85b75b21b009b0f93d7c3fda51e7ae4237c7615
siddhit-kale <ksiddhit9@gmail.com>
2025-02-15 20:11:36 +0530

    Day 39

---
+++ Web_Basic_To_Master/lecture-33/script.js
@@ -0,0 +1,16 @@
+
+console.log("Hello namaste sabhi apka swagat hay!");
+console.log(1+2+3+4+5+6+7+8+9);
+
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString("Hello"));
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString("Easy"));
