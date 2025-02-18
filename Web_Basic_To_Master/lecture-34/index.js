c1278a2e0061173fa68f7343e2c41f55f8912ba3
siddhit-kale <ksiddhit9@gmail.com>
2025-02-18 22:58:27 +0530

    Day 40

---
+++ Web_Basic_To_Master/lecture-34/index.js
@@ -0,0 +1,96 @@
+// var age = 25;
+
+// if(true) {
+//     console.log(age);
+// }
+
+// function solve() {
+//     var age = 25;
+//     console.log(age);
+// }
+// // console.log(age);
+// solve();
+
+// var x = 10;
+// var x = 20;
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// let a = 20;
+// a = 30;
+
+// console.log(a);
+
+// let a = 10;
+// console.log(a);
+// a = "siddhit";
+// console.log(a);
+// a = true;
+// console.log(a);
+// a = null;
+// console.log(a);
+
+// const a = 25;
+// console.log(a);
+
+// a = "sids";
+// console.log(a);
+
+// function sum() {
+//     var a = 20;
+//     var b = 30;
+//     var sum = a + b;
+//     console.log(sum);
+// }
+
+// sum();
+
+// function sumof(a, b) {
+//     var sum = a + b;
+//     console.log(sum);
+// }
+
+// sumof(13, 51);
+
+// function booles(a) {
+//     if(a === true) {
+//         console.log(a);
+//     }
+//     else{
+//         console.log("Answer is the False");
+//     }
+// }
+
+// booles(false);
+
+// let marks = 20.3453;
+// marks = "siddhit";
+// marks = true;
+
+// let marks = "Siddhit";
+// console.log(marks.length);
+
+// let bigint = 12346548461315151516;
+// console.log(bigint);
+
+// console.log(typeof(bigint));
+
+// let person = {
+//     name: "Siddhit",
+//     age: 12,
+//     isstudent: true
+// };
+
+// console.log(person);
+
+// let numbers = [1, 22, 3, 4, 5];
+// console.log(numbers);
+
+// let today = new Date();
+// console.log(today);
+
+let pattern = /hello/;
+console.log(pattern);
