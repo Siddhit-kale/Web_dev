b1e0c858ec3503dacb834e0bd75c5633cbd11642
siddhit-kale <ksiddhit9@gmail.com>
2025-02-19 23:01:43 +0530

    Day 41

---
+++ Web_Basic_To_Master/lecture-35/script.js
@@ -0,0 +1,84 @@
+// console.log("Conditional and operator");
+
+let a = 5;
+let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// console.log(a++); // 5
+// console.log(++a); // 7
+
+// console.log(a--); // 7
+// console.log(--a); // 5
+
+// a += 5;
+// b -= 5;
+// console.log(a, b);
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a === b);
+// console.log(a !== b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// a++;
+// console.log(a > b ? a : b);
+
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!(a && b));
+
+// console.log(false || 'siddhit');
+// console.log(false || 7);
+
+// console.log(a&b);
+// console.log(a|b);
+// console.log(!b);
+// console.log(a >> b);
+// console.log(a << b);
+// console.log(a ^ b);
+// console.log(~(a));
+
+// if(a >= 18) {
+//     console.log('You can vote');
+// }
+// else{
+//     console.log('You cannot vote due to age');
+// }
+
+// if(a >= 18) {
+//     console.log("You can have Driving licence");
+// }
+// else if(a == 18 ) {
+//     console.log("You can applay for Driving licence");
+// }
+
+// else {
+//     console.log("You cannot applay for Driving licence");
+// }
+
+// --a;
+// switch(a) {
+//     case 1: 
+//         console.log('A'); 
+//         break;
+//     case 2:
+//         console.log('B');
+//         break;
+//     case 3:
+//         console.log('C');
+//         break;
+//     case 4:
+//         console.log('D');
+//         break;
+//     default:
+//         console.log('Invalid choice');
+// }
