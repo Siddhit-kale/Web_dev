1d832f96693da822ce0dbec7306f21659d2163dc
siddhit-kale <ksiddhit9@gmail.com>
2025-02-21 22:48:49 +0530

    Day 43

---
+++ Web_Basic_To_Master/lecture-37/script.js
@@ -0,0 +1,76 @@
+// console.log("Plain and Arrow function");
+
+// function printcounting() {
+//     for(let i=1; i<=100; i++) {
+//         console.log(i);
+//     }
+// }
+
+// printcounting();
+
+// function age() {
+//     let name= 'siddhit';
+//     console.log(name);
+//     let age= 18;
+//     console.log(age);
+// }
+
+// age();
+
+// function printNumber(num) {
+//     console.log("Printing number:", num)
+// }
+
+// printNumber(5);
+
+// function getaverage(num1, num2){
+//     let avg = (num1 + num2)/2;
+//     console.log(avg);
+// }
+
+// getaverage(3, 60);
+
+// function getsum(a, b, c){
+//     let sum = a+b+c;
+//     return sum;
+// }
+
+// let ans = getsum(1,2,3);
+// console.log("printing sum: ", ans);
+
+// function getname(firstname, lastname) {
+//     let fullname = firstname + " " + lastname;
+//     return fullname;
+// }
+
+// let fullname = getname("Kale", "siddhit");
+// console.log(fullname);
+
+// function getmultiplication(a,b) {
+//     return a*b;
+// }
+
+// console.log(getmultiplication(2,10));
+
+// const solve = function (a, b) {
+//     return a + b;
+// }
+
+// let ans = solve(2, 10);
+// console.log(ans);
+
+// function squarenumber(num) {
+//     let ans = num ** 2;
+//     return ans;
+// }
+
+// ans = squarenumber(12);
+// console.log(ans);
+
+// let getexp = (a,y) => {
+//     let ans = a*y;
+//     return ans;
+// }
+
+// console.log(getexp(2,10));
+
