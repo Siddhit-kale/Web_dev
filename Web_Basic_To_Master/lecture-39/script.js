5911d57eb5ce40a761dde431658da1ef3d7fefaa
siddhit-kale <ksiddhit9@gmail.com>
2025-02-24 22:02:54 +0530

    Day 45

---
+++ Web_Basic_To_Master/lecture-39/script.js
@@ -0,0 +1,91 @@
+
+
+// saymyname("Siddhit");
+
+// function saymyname(finalname) {
+//     console.log(finalname);
+// }
+
+// console.log(age);
+// var age = 25;
+
+
+// console.log(age);
+// let age = 24;
+
+// console.log(age);
+// const age = 23;
+
+
+// hello();
+// let a = function hello() {
+//     console.log("Hello jae, kaise ho sare");
+// }
+
+// const object1 = new human();
+// class human {
+
+// }
+
+// function solve(number) {
+//     return function(number) {
+//         return number*number;
+//     }
+// }
+
+// let ans = solve(5);
+
+// let fianlans = ans(10);
+
+// console.log(fianlans);
+
+// function greetme(greet, fullname) {
+//     console.log("hello", fullname);
+//     greet();
+// }
+
+
+// let greet = function() {
+//     console.log("Greeting for the day");
+// }
+
+// greetme(greet, "Siddhit");
+
+
+// const arr = [
+//     function(a,b) {
+//         return a+b;
+//     },
+//     function(a,b) {
+//         return a-b;
+//     },
+//     function(a,b) {
+//         return a+b;
+//     }
+
+// ];
+
+// let first = arr[2];
+// let ans = first(5,10);
+// console.log(ans);
+
+
+// let obj = {
+//     age: 25,
+//     wt: 36,
+//     ht: 180,
+//     greet: () => {
+//         console.log("Hello dunia");
+//     }
+// }
+
+// console.log(obj.age);
+// obj.greet();
+
+// var greet;
+// console.log(greet);
+
+// var greet = function() {
+//     console.log("Namaste Dunia");
+// }
+
