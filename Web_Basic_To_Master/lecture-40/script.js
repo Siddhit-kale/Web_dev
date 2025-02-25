9ce6a36ffbe68d0a120d947814ac11a204e1304c
siddhit-kale <ksiddhit9@gmail.com>
2025-02-25 23:01:27 +0530

    Day 46

---
+++ Web_Basic_To_Master/lecture-40/script.js
@@ -0,0 +1,66 @@
+
+// Global Scope
+
+// var age = 15;
+
+// console.log(age);
+
+// {
+//     console.log(age);
+// }
+
+// if(true) {
+//     console.log(age);
+// }
+
+// for(let i=0; i<=2; i++) {
+//     console.log(age);
+// }
+
+// function sayhello() {
+//     console.log("HI",age);
+// }
+// sayhello();
+
+
+// Function Scope
+
+// function sayhello() {
+//     var name = "Earth";
+//     console.log("HI", name);
+// }
+
+// console.log("Hi", name); // Error
+// sayhello();
+
+
+// Block scope
+
+// console.log(height);
+// {
+//     var height = 180;
+// }
+
+// console.log(height);
+
+// {
+//     let height = 180;
+//     console.log(height);
+// }
+
+// console.log(height); // error
+
+// {
+//     const height=180;
+// }
+
+// console.log(height); // Error
+
+
+// Temporal Dead Zone
+
+// console.log(marks); // error
+// let marks = 100;
+// console.log(marks);
+
+
