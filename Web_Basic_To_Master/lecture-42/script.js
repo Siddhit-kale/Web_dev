a5f9e9f2968042d37472ac723b09d8851e172ab5
siddhit-kale <ksiddhit9@gmail.com>
2025-02-28 22:45:28 +0530

    Day 48

---
+++ Web_Basic_To_Master/lecture-42/script.js
@@ -0,0 +1,44 @@
+// console.log("common object");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(60,11,2,3,45,99));
+
+
+// console.log(Math.min(44,223,56,1,6,4,5));
+
+// console.log(Math.round(1.4));
+
+// console.log(Math.floor(1.4));
+
+// console.log(Math.floor(1.4));
+
+// console.log(Math.ceil(1.1));
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(5));
+
+// console.log(Math.pow(2,10));
+
+// console.log(Date);
+
+
+let curr = new Date();
+// console.log(curr);
+
+// let date = new Date('septembar 12 2004 07:00');
+// console.log(date);
+
+// let newDate = new Date(1998, 8, 20, 7);
+
+// console.log( newDate);
+
+// console.log(curr.getFullYear());
+
+// console.log(curr.getTimezoneOffset());
+
+// console.log(curr.setUTCDate());
+
