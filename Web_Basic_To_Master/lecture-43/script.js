58f0650ef9f5f2e81ef011ea86f692980d650408
siddhit-kale <ksiddhit9@gmail.com>
2025-03-01 19:00:44 +0530

    Day 49

---
+++ Web_Basic_To_Master/lecture-43/script.js
@@ -0,0 +1,37 @@
+// // console.log("object cloning and garbage collector");
+
+// let obj = {
+//     age:12,
+//     wt:68,
+//     ht:180
+// };
+
+// // obj.color = "white";
+
+// // let dest = {...obj};
+
+// // let obj2 = {
+// //     value:101,
+// //     name: "Love babbar"
+// // }
+
+
+// // // console.log(dest);
+
+// // let dest = Object.assign({}, obj, obj2);
+// // console.log(dest);
+
+// let dest = {};
+
+// for(let key in obj) {
+//     let newkey = key;
+//     let newValue = obj[key];
+//     // insert newkey and value in dest and create clone
+//     dest[newkey] = newValue;
+// }
+
+// console.log(dest);
+// obj.age = 90;
+// console.log(obj);
+
+
