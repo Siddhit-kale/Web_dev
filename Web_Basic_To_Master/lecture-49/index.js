b6a402df55d95a9994f7075e9a036de1762be695
siddhit-kale <ksiddhit9@gmail.com>
2025-03-14 21:23:22 +0530

    Day 55

---
+++ Web_Basic_To_Master/lecture-49/index.js
@@ -0,0 +1,29 @@
+// console.log("Start");
+
+
+// setTimeout(function timeout() {
+//     console.log("Love code");
+// }, 5000);
+
+// console.log("End");
+
+
+// console.log("start");
+
+// setTimeout(function timeout() {
+//     console.log("this second time");
+// }, 6000);
+
+// console.log("End"); 
+
+$.on('button', 'click', function() {
+    console.log('You clicked the button');
+});
+
+console.log("HI!");
+
+setTimeout(function timeout() {
+    console.log('Click the button');
+}, 5000);
+
+console.log("welcome to loupe.");
