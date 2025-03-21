78cdada2c59c492209642a094146d140c29fbe20
siddhit-kale <ksiddhit9@gmail.com>
2025-03-21 23:10:23 +0530

    Day 60

--- Web_Basic_To_Master/lecture-53/index.js
+++ Web_Basic_To_Master/lecture-53/index.js
@@ -1,6 +1,8 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-    
-    const searchButton = document.getElementById("search-button");
+
+
+    const searchButton = document.getElementById("search-btn");
     const usernameInput = document.getElementById("user-input");
     const statsContainer = document.querySelector(".stats-container");
     const easyProgressCircle = document.querySelector(".easy-progress");
@@ -11,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const hardLabel = document.getElementById("hard-label");
     const cardStatsContainer = document.querySelector(".stats-cards");
 
+    //return true or false based on a regex
     function validateUsername(username) {
         if(username.trim() === "") {
             alert("Username should not be empty");
@@ -25,20 +28,86 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     async function fetchUserDetails(username) {
-        const url = 'https://leetcode-stats-api.herokuapp.com/${username}',
-        
-        try {
 
-            searchButton.textContent = "searching...";
+        try{
+            searchButton.textContent = "Searching...";
             searchButton.disabled = true;
+            //statsContainer.classList.add("hidden");
 
-            const response = await fetch(url);
+            // const response = await fetch(url);
+            const proxyUrl = 'https://cors-anywhere.herokuapp.com/' 
+            const targetUrl = 'https://leetcode.com/graphql/';
+            
+            const myHeaders = new Headers();
+            myHeaders.append("content-type", "application/json");
+
+            const graphql = JSON.stringify({
+                query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
+                variables: { "username": `${username}` }
+            })
+            const requestOptions = {
+                method: "POST",
+                headers: myHeaders,
+                body: graphql,
+            };
+
+            const response = await fetch(proxyUrl+targetUrl, requestOptions);
             if(!response.ok) {
-                throw new Error("Unable to fecth the user details");
+                throw new Error("Unable to fetch the User details");
             }
-            const data = await response.json();
-            console.log("Logging data: ", data);
+            const parsedData = await response.json();
+            console.log("Logging data: ", parsedData) ;
+
+            displayUserData(parsedData);
         }
+        catch(error) {
+            statsContainer.innerHTML = `<p>${error.message}</p>`
+        }
+        finally {
+            searchButton.textContent = "Search";
+            searchButton.disabled = false;
+        }
+    }
+
+    function updateProgress(solved, total, label, circle) {
+        const progressDegree = (solved/total)*100;
+        circle.style.setProperty("--progress-degree", `${progressDegree}%`);
+        label.textContent = `${solved}/${total}`;
+    }
+
+
+    function displayUserData(parsedData) {
+        const totalQues = parsedData.data.allQuestionsCount[0].count;
+        const totalEasyQues = parsedData.data.allQuestionsCount[1].count;
+        const totalMediumQues = parsedData.data.allQuestionsCount[2].count;
+        const totalHardQues = parsedData.data.allQuestionsCount[3].count;
+
+        const solvedTotalQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[0].count;
+        const solvedTotalEasyQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[1].count;
+        const solvedTotalMediumQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[2].count;
+        const solvedTotalHardQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[3].count;
+
+        updateProgress(solvedTotalEasyQues, totalEasyQues, easyLabel, easyProgressCircle);
+        updateProgress(solvedTotalMediumQues, totalMediumQues, mediumLabel, mediumProgressCircle);
+        updateProgress(solvedTotalHardQues, totalHardQues, hardLabel, hardProgressCircle);
+
+        const cardsData = [
+            {label: "Overall Submissions", value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[0].submissions },
+            {label: "Overall Easy Submissions", value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[1].submissions },
+            {label: "Overall Medium Submissions", value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[2].submissions },
+            {label: "Overall Hard Submissions", value:parsedData.data.matchedUser.submitStats.totalSubmissionNum[3].submissions },
+        ];
+
+        console.log("card ka data: " , cardsData);
+
+        cardStatsContainer.innerHTML = cardsData.map(
+            data => 
+                    `<div class="card">
+                    <h4>${data.label}</h4>
+                    <p>${data.value}</p>
+                    </div>`
+        ).join("")
+
     }
 
     searchButton.addEventListener('click', function() {
