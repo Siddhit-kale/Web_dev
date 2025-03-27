4cc41f6556c888d08b6a8bfd376429ed088577dd
siddhit-kale <ksiddhit9@gmail.com>
2025-03-27 18:46:30 +0530

    Day 61

---
+++ Web_Basic_To_Master/React_app/introReact/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>My name is Kale Siddhit.</h1>
+      <h3>I am currenlty a student in charusat.</h3>
+    </>
+  )
+}
+
+export default App
